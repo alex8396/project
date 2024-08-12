1ca17787e7ef9aefdb5770a4ab1a945f21621beb
alex8396 <alex8396@naver.com>
2024-08-12 23:14:34 +0900

    dd

--- src/hooks/useDiary.js
+++ src/hooks/useDiary.js
@@ -1,6 +1,7 @@
 import { useContext, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { DiaryStateContext } from "../App";
+
 const useDiary = (id) => {
   const data = useContext(DiaryStateContext);
   const [diary, setDiary] = useState();
@@ -14,8 +15,9 @@ const useDiary = (id) => {
       alert("경기후기가 존재하지 않습니다");
       navigate("/", { replace: true });
     }
-  }, [id, data]);
+  }, [id, data, navigate]);  // navigate를 종속성 배열에 추가합니다.
 
   return diary;
 };
+
 export default useDiary;
